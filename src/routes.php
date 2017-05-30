896b0bc59c3e9b939e1375a80225d3c7bd57dda7
nadirberkoun <nberkoun@orchestra-premaman.com>
2017-05-30 17:11:09 +0200

    registerBDD

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,15 @@
 <?php
-// Routes
 
+// Routes
+function initDb() {
+    try {
+        return new PDO('mysql:host=localhost;dbname=ecolight', 'root', '');
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
+}
+
+////////////////////   Index   /////////////////////////////////////////////////
 
 $app->get('/', function ($request, $response, $args) {
     // Sample log message
@@ -9,7 +18,7 @@ $app->get('/', function ($request, $response, $args) {
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
-
+///////////////////   Enregistrement   /////////////////////////////////////////
 
 $app->get('/register', function ($request, $response, $args) {
     // Render index view
@@ -17,7 +26,7 @@ $app->get('/register', function ($request, $response, $args) {
     return $this->renderer->render($response, 'register.phtml', $args);
 });
 
-$app->get('/doRegister', function ($request, $response, $args) {
+$app->post('/doRegister', function ($request, $response, $args) {
     // if session redirect to dashboard
     $profil = [
         ':login' => $request->getParam('login'),
@@ -30,13 +39,13 @@ $app->get('/doRegister', function ($request, $response, $args) {
         ':cp' => $request->getParam('cp'),
         ':tel' => $request->getParam('tel')
     ];
-    /*if ($app->request->post('password') === $app->request->post('password_confirm')) {
+    $dbh = initDb();
+    if ($request->getParam('password') === $request->getParam('password_confirm')) {
         $sql = "INSERT INTO `Utilisateur` (`id_util`, `login_util`, `mdp_util`, `mail_util`, `nom_util`, `prenom_util`, `adresse_util`, `ville_util`, `cp_util`, `tel_util`) VALUES (null, :login, :password, :email, :nom, :prenom, :adresse, :ville, :cp, :tel)";
         $req = $dbh->prepare($sql);
-        $result = $req->execute($tab);
+        $result = $req->execute($profil);
         $doRegister = true;
-    }*/
-    $doRegister = true;
+    }
     if ($doRegister) {
         $_SESSION['hello'] = 'hello tout va bien';
         return $response->withRedirect('login');
@@ -46,11 +55,17 @@ $app->get('/doRegister', function ($request, $response, $args) {
     }
 });
 
-
+///////////////////   Connexion   //////////////////////////////////////////////
 
 $app->get('/login', function ($request, $response, $args) {
     // Render index view
-    return $this->renderer->render($response, 'login.phtml', $args);
+
+    $displayTemplate = $this->renderer->render($response, 'login.phtml', [
+            'hello' => $_SESSION['hello'],
+            'message' => 'Connectez vous avec votre compte'
+            ]);
+    unset($_SESSION['hello']);
+    return $displayTemplate;
 });
 
 $app->post('/doLogin', function ($request, $response, $args) {
@@ -58,9 +73,7 @@ $app->post('/doLogin', function ($request, $response, $args) {
     return $this->renderer->render($response, 'login.phtml', $args);
 });
 
-
-
-
+//////////////////   Changement MDP   //////////////////////////////////////////
 
 $app->get('/new_mdp', function ($request, $response, $args) {
     // Render index view
@@ -71,3 +84,5 @@ $app->post('/doNew_mdp', function ($request, $response, $args) {
     // Render index view
     return $this->renderer->render($response, 'new_mdp.phtml', $args);
 });
+
+////////////////////////////////////////////////////////////////////////////////
